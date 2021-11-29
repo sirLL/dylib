f71a98f97a87129d0e24eee1163f9f701476f4e7
liuliang <1725790539@qq.com>
2021-11-29 15:07:14 +0800

    BUG

--- lib_base/src/main/java/com/wareroom/lib_base/api/BaseURLConfig.java
+++ lib_base/src/main/java/com/wareroom/lib_base/api/BaseURLConfig.java
@@ -1,7 +1,7 @@
 package com.wareroom.lib_base.api;
 
 public class BaseURLConfig {
-//    public static final String BASE_URL="http://api.dyhm.shop/";
-    public static final String BASE_URL="http://apitest.dyhm.shop/";
+    public static final String BASE_URL="http://api.dyhm.shop/";
+//    public static final String BASE_URL="http://apitest.dyhm.shop/";
 
 }
